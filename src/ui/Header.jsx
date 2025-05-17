de06ba31c92c9f53ae452144f723989cc4011a23
Youssef <youssefyaser2020@gmail.com>
2025-05-18 02:56:30 +0300

    Refactor routing structure to include AppLayout and add Header and CartOverview components

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router';
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza Co.</Link>
+      <p>HI</p>
+    </header>
+  );
+}
+
+export default Header;
