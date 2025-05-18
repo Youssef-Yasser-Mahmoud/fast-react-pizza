7fce11073eda2309a385761a359d69381db62a4e
Youssef <youssefyaser2020@gmail.com>
2025-05-18 04:58:52 +0300

    Add error handling component and integrate it into routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import Home from './ui/Home';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
-import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: '/cart',
