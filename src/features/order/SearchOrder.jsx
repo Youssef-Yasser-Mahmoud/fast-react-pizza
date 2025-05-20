242fb307022768afa13d3c14ba54422ec6c07c7b
Youssef <youssefyaser2020@gmail.com>
2025-05-20 19:41:32 +0300

    Refactor SearchOrder component to use 'react-router-dom' for navigation and enhance input styling with Tailwind CSS classes, including a placeholder for better user guidance.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
@@ -13,7 +14,12 @@ function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
+      <input
+        placeholder='Search order #'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className='w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72'
+      />
     </form>
   );
 }
