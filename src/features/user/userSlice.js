dd669c039a4bd83592321feda5b64350e18f7ab2
Youssef <youssefyaser2020@gmail.com>
2025-05-22 18:43:15 +0300

    fixing bug by renaming state property from 'name' to 'userName' in userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -9,7 +9,7 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     updateName(state, action) {
-      state.name = action.payload;
+      state.userName = action.payload;
     },
   },
 });
