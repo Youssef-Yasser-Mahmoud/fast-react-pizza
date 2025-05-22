12fe3ac414aa82efb1fa94e046d94d015acac777
Youssef <youssefyaser2020@gmail.com>
2025-05-22 18:37:36 +0300

    Update Username component to conditionally render based on userName availability, returning null if not present for improved user experience.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,6 +2,9 @@ import { useSelector } from 'react-redux';
 
 function Username() {
   const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+
   return <div className='hidden text-sm font-semibold md:block'>{userName}</div>;
 }
 
